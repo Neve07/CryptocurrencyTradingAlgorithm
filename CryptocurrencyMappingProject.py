b4ea62be5860310ca4f79e2e011e891b632fbb95
Neve07 <50306624+Neve07@users.noreply.github.com>
2022-04-11 18:56:48 -0400

    Add files via upload

---
+++ CryptocurrencyMappingProject.py
@@ -0,0 +1,203 @@
+###################
+# Cryptocurrency Algorithm
+# This project is solely to explore and apply my learnings related to cryptocurrency data
+# Different sections build on each other, adding more elements as I learned more
+####################
+
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.dates import DateFormatter
+
+# Retrieve two weeks of Bitcoin to USD exchange rates with a 1 hour interval and save the dataframe to a variable.
+BTC_USD = yf.download("BTC-USD", start='2020-01-01', end='2020-12-31', interval='1d')
+BTC_USD.head()
+
+# SIMPLE PRICE CHART - STEP ONE ########################################################################
+# fig, ax = plt.subplots(dpi=500)
+
+# date_format = DateFormatter("%h-%d-%y")
+# ax.xaxis.set_major_formatter(date_format)
+# ax.tick_params(axis='x', labelsize=4)
+# fig.autofmt_xdate()
+
+# ax.plot(BTC_USD['Close'], lw=0.75)
+
+# ax.set_ylabel('Price of Bitcoin (USD)')
+# ax.set_title('Bitcoin to USD Exchange Rate')
+# ax.grid() # adding a grid
+
+# plt.show()
+
+# #9-day Simple Moving Average with pandas
+# BTC_USD['SMA_9'] = BTC_USD['Close'].rolling(window=9, min_periods=1).mean()
+# BTC_USD['SMA_30'] = BTC_USD['Close'].rolling(window=30, min_periods=1).mean()
+
+# # Display the last 5 entries of the dataframe
+# BTC_USD.tail()
+
+#Plotting Simple Moving Averages #########################################################################
+# fig, ax = plt.subplots(dpi=500)
+
+# # Formatting the date axis
+# date_format = DateFormatter("%h-%d-%y")
+# ax.xaxis.set_major_formatter(date_format)
+# ax.tick_params(axis='x', labelsize=8)
+# fig.autofmt_xdate()
+
+# # Plotting the closing price against the date (1 day interval)
+# ax.plot(BTC_USD['Close'], lw=0.75, label='Closing Price') 
+
+
+# # labels and title on plot
+# ax.set_ylabel('Bitcoin Price (USD)')
+# ax.set_title('Bitcoin --> USD Exchange Rate')
+# ax.grid() 
+# ax.legend() 
+
+# plt.show()
+
+
+
+# Create a pandas dataframe,same size as the BTC_USD dataframe,covers the same dates
+trade_signals = pd.DataFrame(index=BTC_USD.index)
+
+# intervals for the Fast and Slow Simple Moving Averages (in days)
+short_interval = 10
+long_interval = 40
+
+# Compute Simple Moving Averages and add to the dateframe
+trade_signals['Short'] = BTC_USD['Close'].rolling(window=short_interval, min_periods=1).mean()
+trade_signals['Long'] = BTC_USD['Close'].rolling(window=long_interval, min_periods=1).mean()
+
+# Create a new column populated with zeros
+trade_signals['Signal'] = 0.0
+
+# Wherever the Shorter term SMA is above the Longer term SMA, set the Signal column to 1, otherwise 0
+trade_signals['Signal'] = np.where(trade_signals['Short'] > trade_signals['Long'], 1.0, 0.0)   
+trade_signals['Position'] = trade_signals['Signal'].diff()
+
+fig, ax = plt.subplots(dpi=500)
+
+# Formatting the date axis
+date_format = DateFormatter("%h-%d-%y")
+ax.xaxis.set_major_formatter(date_format)
+ax.tick_params(axis='x', labelsize=8)
+fig.autofmt_xdate()
+
+
+# Plotting Bitcoin closing price against the date (1 day interval)
+ax.plot(BTC_USD['Close'], lw=0.75, label='Closing Price')
+
+# shorter-term moving average
+ax.plot(trade_signals['Short'], lw=0.75, alpha=0.75, color='orange', label='Short-term SMA')
+
+# longer-term moving average
+ax.plot(trade_signals['Long'], lw=0.75, alpha=0.75, color='purple', label='Long-term SMA')
+
+
+# green arrows for buy orders
+ax.plot(trade_signals.loc[trade_signals['Position']==1.0].index, trade_signals.Short[trade_signals['Position'] == 1.0],
+ marker=6, ms=4, linestyle='none', color='green')
+
+ # red arrows for sell orders
+ax.plot(trade_signals.loc[trade_signals['Position'] == -1.0].index, trade_signals.Short[trade_signals['Position'] == -1.0],
+ marker=7, ms=4, linestyle='none', color='red')
+
+
+# Adding labels and title to the plot
+ax.set_ylabel('Price of Bitcoin (USD)')
+ax.set_title('Bitcoin to USD Exchange Rate')
+ax.grid() 
+ax.legend()
+
+plt.show()
+
+##################################################################################################
+
+trade_signals = pd.DataFrame(index=BTC_USD.index)
+
+short_interval = 10
+long_interval = 40
+
+# Compute the Simple Moving Averages and add it to the dateframe as new columns
+trade_signals['Short'] = BTC_USD['Close'].rolling(window=short_interval, min_periods=1).mean()
+trade_signals['Long'] = BTC_USD['Close'].rolling(window=long_interval, min_periods=1).mean()
+
+# Create a new column populated with zeros
+trade_signals['Signal'] = 0.0
+
+# Wherever the Shorter term SMA is above the Longer term SMA, set the Signal column to 1, otherwise 0
+trade_signals['Signal'] = np.where(trade_signals['Short'] > trade_signals['Long'], 1.0, 0.0)   
+trade_signals['Position'] = trade_signals['Signal'].diff()
+
+fig, ax = plt.subplots(dpi=500)
+
+# Formatting the date axis
+date_format = DateFormatter("%h-%d-%y")
+ax.xaxis.set_major_formatter(date_format)
+ax.tick_params(axis='x', labelsize=8)
+fig.autofmt_xdate()
+
+# Bitcoin closing price against the date (1 day interval)
+ax.plot(BTC_USD['Close'], lw=0.75, label='Closing Price')
+
+# shorter-term moving average
+ax.plot(trade_signals['Short'], lw=0.75, alpha=0.75, color='orange', label='Short-term SMA')
+# longer-term moving average
+ax.plot(trade_signals['Long'], lw=0.75, alpha=0.75, color='purple', label='Long-term SMA')
+
+# green arrows for buy orders
+ax.plot(trade_signals.loc[trade_signals['Position']==1.0].index, trade_signals.Short[trade_signals['Position'] == 1.0],
+ marker=6, ms=4, linestyle='none', color='green')
+
+ # red arrows for sell orders
+ax.plot(trade_signals.loc[trade_signals['Position'] == -1.0].index, trade_signals.Short[trade_signals['Position'] == -1.0],
+ marker=7, ms=4, linestyle='none', color='red')
+
+
+# Adding labels and title to the plot
+ax.set_ylabel('Price of Bitcoin (USD)')
+ax.set_title('Bitcoin to USD Exchange Rate')
+ax.grid()
+ax.legend()
+
+plt.show()
+
+#TESTING ###################################################################################
+initial_balance = 10000.0 
+
+# dataframe containing all the dates considered
+backtest = pd.DataFrame(index=trade_signals.index)
+
+# Add column containing the daily percent returns of Bitcoin
+backtest['BTC_Return'] = BTC_USD['Close'] / BTC_USD['Close'].shift(1) # Current closing price / yesterday's closing price
+
+# Add column containing the daily percent returns of the Moving Average Crossover strategy
+backtest['Alg_Return'] = np.where(trade_signals.Signal == 1, backtest.BTC_Return, 1.0)
+
+# Add column containing the daily value of the portfolio using the Crossover strategy
+backtest['Balance'] = initial_balance * backtest.Alg_Return.cumprod() # cumulative product
+
+fig, ax = plt.subplots(dpi=500)
+
+# Formatting the date axis
+date_format = DateFormatter("%h-%d-%y")
+ax.xaxis.set_major_formatter(date_format)
+ax.tick_params(axis='x', labelsize=8)
+fig.autofmt_xdate()
+
+# value of Buy and Hold Strategy
+ax.plot(initial_balance*backtest.BTC_Return.cumprod(), lw=0.75, alpha=0.75, label='Buy and Hold')
+
+# total value of Crossing Averages Strategy
+ax.plot(backtest['Balance'], lw=0.75, alpha=0.75, label='Crossing Averages')
+
+
+ax.set_ylabel('USD')
+ax.set_title('Value of Portfolio')
+ax.grid() 
+ax.legend()
+
+plt.show()
